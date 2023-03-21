1a331a4e8824ba082a1190e1100a00da2cb8f318
juviscript <juvipana@gmail.com>
2023-03-21 15:41:10 -0400

    Created User, VideoGame, and UserProfile classes

---
+++ Application/src/main/java/com/kenzie/appserver/service/model/UserProfile.java
@@ -0,0 +1,12 @@
+package com.kenzie.appserver.service.model;
+
+import java.util.LinkedList;
+
+public class UserProfile {
+
+    private User user;
+    LinkedList<VideoGame> favoriteGames;
+    LinkedList<VideoGame> gamesPlayed;
+
+
+}
