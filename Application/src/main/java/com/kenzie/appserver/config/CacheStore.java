5628ee45f0f655ad05d4a2de75cfb35ae427f75c
melijohnson19 <110068494+melijohnson19@users.noreply.github.com>
2023-03-29 18:20:14 -0400

    Update CacheStore.java

--- Application/src/main/java/com/kenzie/appserver/config/CacheStore.java
+++ Application/src/main/java/com/kenzie/appserver/config/CacheStore.java
@@ -1,2 +1,33 @@
-package com.kenzie.appserver.config;public class CacheStore {
+package com.kenzie.appserver.config;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.kenzie.appserver.service.model.VideoGame;
+
+import java.util.concurrent.TimeUnit;
+
+public class CacheStore {
+    private Cache<String, VideoGame> cache;
+
+    public CacheStore(int expiry, TimeUnit timeUnit) {
+        this.cache = CacheBuilder.newBuilder()
+                .expireAfterWrite(expiry, timeUnit)
+                .concurrencyLevel(Runtime.getRuntime().availableProcessors())
+                .build();
+    }
+        public VideoGame get(String key) {
+        return cache.getIfPresent(key);
+    }
+
+    public void evict(String key) {
+        if (key != null) {
+            cache.invalidate(key);
+        }
+    }
+
+    public void add(String key, VideoGame value) {
+        if (key != null) {
+            cache.put(key, value);
+        }
+    }
 }
