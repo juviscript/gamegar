ea00b37e7a3b15e79bf735799e9531abda187373
melijohnson19 <110068494+melijohnson19@users.noreply.github.com>
2023-03-30 19:33:26 -0400

    Update UserService.java

--- Application/src/main/java/com/kenzie/appserver/service/UserService.java
+++ Application/src/main/java/com/kenzie/appserver/service/UserService.java
@@ -1,7 +1,110 @@
 package com.kenzie.appserver.service;
 
+import com.kenzie.appserver.config.CacheStore;
+import com.kenzie.appserver.config.CacheStoreUser;
+import com.kenzie.appserver.repositories.UserRepository;
+import com.kenzie.appserver.repositories.model.UserRecord;
+import com.kenzie.appserver.service.model.User;
+import com.kenzie.appserver.service.model.VideoGame;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    private CacheStoreUser cache;
+
+    public UserService(UserRepository userRepository, CacheStoreUser cache) {
+        this.userRepository = userRepository;
+        this.cache = cache;
+    }
+
+    public User findUserById (String id) {
+
+        User userFromService = userRepository
+                .findById(id)
+                .map(users -> new User(users.getUserId(),
+                        users.getName(),
+                        users.getEmail(),
+                        users.getUsername(),
+                        users.getBirthday()))
+                .orElse(null);
+
+        return userFromService;
+    }
+
+    public User findUserByName (String names) {
+
+        User nameFromService = userRepository
+                .findById(names)
+                .map(users -> new User(users.getName(),
+                        users.getUserId(),
+                        users.getEmail(),
+                        users.getUsername(),
+                        users.getBirthday()))
+                .orElse(null);
+
+        return nameFromService;
+    }
+
+    public User addNewUser(User user){
+        UserRecord userRecord = new UserRecord();
+
+        userRecord.setUserId(user.getUserId());
+        userRecord.setUsername(user.getUsername());
+        userRecord.setName(user.getName());
+        userRecord.setEmail(user.getEmail());
+        userRecord.setBirthday(user.getBirthday());
+
+        return user;
+    }
+
+    public List<User> findAllUsers() {
+        List<User> users = new ArrayList<>();
+
+        Iterable<UserRecord> userIterator = userRepository.findAll();
+        for(UserRecord record : userIterator) {
+            users.add(new User (record.getUserId(),
+                    record.getUsername(),
+                    record.getName(),
+                    record.getEmail(),
+                    record.getBirthday()));
+        }
+        return users;
+    }
+
+    public void updateUser (User user) {
+        if (userRepository.existsById(user.getUserId())) {
+            UserRecord userRecord = new UserRecord();
+            userRecord.setUserId(user.getUserId());
+            userRecord.setUsername(user.getUsername());
+            userRecord.setName(user.getName());
+            userRecord.setEmail(user.getEmail());
+            userRecord.setBirthday(user.getBirthday());
+            userRepository.save(userRecord);
+        }
+        User userFromCache = null;
+        userFromCache = cache.get(user.getUserId());
+        if(userFromCache != null){
+            cache.evict(userFromCache.getUserId());
+        }
+
+    }
+
+    public void deleteUserById(String userID){
+        User user = null;
+        if (userID != null) {
+            user  = cache.get(userID);
+        } else {
+            throw new IllegalArgumentException("User ID not valid.");
+        }
+        if (user  != null) {
+            cache.evict(user.getUserId());
+        }
+    }
+
 }
