2804772e39e2c7bf764939067d11020594b51ffa
melijohnson19 <110068494+melijohnson19@users.noreply.github.com>
2023-03-31 13:08:06 -0400

    Update User.java

--- Application/src/main/java/com/kenzie/appserver/service/model/User.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/User.java
@@ -4,35 +4,25 @@ import java.time.LocalDate;
 
 public class User {
 
-<<<<<<< HEAD
-=======
     private String userId;
->>>>>>> origin/juvisbranch
     private String name;
     private String email;
     private String username;
 
     private LocalDate birthday;
 
-<<<<<<< HEAD
-    public User(String name, String email, String username) {
-=======
     public User(String userId, String name, String email, String username, LocalDate birthday) {
         this.userId = userId;
->>>>>>> origin/juvisbranch
         this.name = name;
         this.email = email;
         this.username = username;
         this.birthday = birthday;
     }
 
-<<<<<<< HEAD
-=======
     public String getUserId() {
         return userId;
     }
 
->>>>>>> origin/juvisbranch
     public String getName() {
         return name;
     }
