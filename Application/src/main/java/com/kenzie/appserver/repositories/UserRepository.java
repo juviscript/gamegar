01ae72922228f2e29a33143021f4ad7160868745
melijohnson19 <melissa.johnson@mykenzie.snhu.edu>
2023-03-31 15:53:10 -0400

    Changed User Repository

--- Application/src/main/java/com/kenzie/appserver/repositories/UserRepository.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.kenzie.appserver.repositories;
 
+import com.kenzie.appserver.repositories.model.UserRecord;
 import com.kenzie.appserver.service.model.User;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
