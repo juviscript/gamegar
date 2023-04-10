1c6fb05fb176b2fa988bdd07447a060b771f3246
melijohnson19 <110068494+melijohnson19@users.noreply.github.com>
2023-04-10 19:45:23 -0400

    Update gameAdmin.js
    Changes to gameAdmin

--- Frontend/src/pages/gameAdmin.js
+++ Frontend/src/pages/gameAdmin.js
@@ -43,7 +43,7 @@ class GameAdmin extends BaseClass {
     // Render Methods --------------------------------------------------------------------------------------------------
 
     renderGames() {
-        let gameHtml = "";
+        let gameHtml = "";                              // Variable named gameHtml that equals an empty string
         const games = this.dataStore.get("games");
 
         if (games.length <= 0) {
@@ -59,13 +59,11 @@ class GameAdmin extends BaseClass {
                                 <li>Year: ${game.year}</li>
                             </ul>
                         </div>
-
                         <div id="info-2">
                             <li>Genre: ${game.genre}</li>
                             <li>Platforms: ${game.platforms}</li>
                             <li>Tags: ${game.tags}</li>
                         </div>
-
                         <div id="description">
                             <p>
                                 ${game.description}
@@ -78,7 +76,7 @@ class GameAdmin extends BaseClass {
         }
 
 
-        document.getElementById("game-list").innerHTML = gameHtml;
+        document.getElementById("games-list").innerHTML = gameHtml;
     }
 
 
@@ -88,36 +86,6 @@ class GameAdmin extends BaseClass {
     onRefresh() {
         this.fetchGames();
     }
-
-    /**
-     * Method to run when the create playlist submit button is pressed. Call the MusicPlaylistService to create the
-     * playlist.
-    //  */
-    // async onSubmit(event) {
-    //     // Prevent the form from refreshing the page
-    //     event.preventDefault();
-    //
-    //     // Set the loading flag
-    //     let createButton = document.getElementById('create-concert');
-    //     createButton.innerText = 'Loading...';
-    //     createButton.disabled = true;
-    //
-    //     // Get the values from the form inputs
-    //     const concertName = document.getElementById('concert-name').value;
-    //     const date = document.getElementById('date').value;
-    //     const baseTicketPrice = document.getElementById('ticket-price').value;
-    //
-    //     // Create the concert
-    //     const concert = await this.client.createConcert(concertName, date, baseTicketPrice);
-    //
-    //     // Reset the form
-    //     document.getElementById("create-playlist-form").reset();
-    //
-    //     // Re-enable the form
-    //     createButton.innerText = 'Create';
-    //     createButton.disabled = false;
-    //     this.onRefresh();
-    // }
 
     async onSubmit(game) {
         // Prevent the form from refreshing the page
@@ -149,7 +117,6 @@ class GameAdmin extends BaseClass {
         createButton.disabled = false;
         this.onRefresh();
     }
-
 }
 
 /**
@@ -161,3 +128,134 @@ const main = async () => {
 };
 
 //window.addEventListener('DOMContentLoaded', main);
+import BaseClass from '../util/baseClass';
+import DataStore from '../util/DataStore';
+import VideoGameClient from "../api/videoGameClient";
+
+/**
+ * Logic needed for the create playlist page of the website.
+ */
+class GameAdmin extends BaseClass {
+    constructor() {
+        super();
+        this.bindClassMethods(['onSubmit', 'onRefresh', 'renderGames'], this);
+        this.dataStore = new DataStore();
+    }
+
+    /**
+     * Once the page has loaded, set up the event handlers and fetch the concert list.
+     */
+    mount() {
+        document.getElementById('refresh').addEventListener('click', this.onRefresh);
+        document.getElementById('create-game-form').addEventListener('submit', this.onSubmit);
+
+        this.client = new VideoGameClient();
+        this.dataStore.addChangeListener(this.renderGames)
+        this.fetchGames();
+    }
+
+    async fetchGames() {
+        const games = await this.client.getAllGames(this.errorHandler)
+
+        if (games && games.length > 0) {
+            for (const game of games) {
+                games.list = await this.fetchGames(game.id);
+            }
+        }
+        this.dataStore.set("games", games);
+    }
+
+    async fetchGames(id) {
+        return await this.client.getAllGames(id, this.errorHandler);
+    }
+
+
+    // Render Methods --------------------------------------------------------------------------------------------------
+
+    renderGames() {
+        let gameHtml = "";                              // Variable named gameHtml that equals an empty string
+        const games = this.dataStore.get("games");
+
+        if (games.length <= 0) {
+            for (const game of games) {
+
+                gameHtml += `
+                    <div class="card">
+                        <h2> ${game.title} </h2>
+                        <div id="info-1">
+                            <ul>
+                                <li>Developer: ${game.developer}</li>
+                                <li>Country of Origin: ${game.country}</li>
+                                <li>Year: ${game.year}</li>
+                            </ul>
+                        </div>
+                        <div id="info-2">
+                            <li>Genre: ${game.genre}</li>
+                            <li>Platforms: ${game.platforms}</li>
+                            <li>Tags: ${game.tags}</li>
+                        </div>
+                        <div id="description">
+                            <p>
+                                ${game.description}
+                            </p>
+                        </div>
+                `;
+            }
+        } else {
+            gameHtml = `<div> There are no games... :( </div>`;
+        }
+
+
+        document.getElementById("games-list").innerHTML = gameHtml;
+    }
+
+
+
+    // Event Handlers --------------------------------------------------------------------------------------------------
+
+    onRefresh() {
+        this.fetchGames();
+    }
+
+    async onSubmit(game) {
+        // Prevent the form from refreshing the page
+        game.preventDefault();
+
+        // Set the loading flag
+        let createButton = document.getElementById('create-game');
+        createButton.innerText = 'Loading...';
+        createButton.disabled = true;
+
+        // Get the values from the form inputs
+        const title = document.getElementById('title').value;
+        const developer = document.getElementById('developer').value;
+        const genre = document.getElementById('genre').value;
+        const year = document.getElementById('year').value;
+        const description = document.getElementById('description').value;
+        const country = document.getElementById('country').value;
+        const platforms = document.getElementById('platforms').value;
+        const tags = document.getElementById('tags').value;
+
+        // Create the concert
+        const games = await this.client.createGame(title,developer,genre,year,description,country,platforms,tags);
+
+        // Reset the form
+        document.getElementById("create-game-list").reset();
+
+        // Re-enable the form
+        createButton.innerText = 'Create';
+        createButton.disabled = false;
+        this.onRefresh();
+    }
+}
+
+/**
+ * Main method to run when the page contents have loaded.
+ */
+const main = async () => {
+    const gameAdmin = new GameAdmin();
+    gameAdmin.mount();
+};
+
+//window.addEventListener('DOMContentLoaded', main);
+
