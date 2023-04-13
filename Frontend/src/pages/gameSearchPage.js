e42256cc59490979990544a60a463fd19dff4faa
melijohnson19 <melissa.johnson@mykenzie.snhu.edu>
2023-04-13 13:37:00 -0400

    changed Devs page

--- Frontend/src/pages/gameSearchPage.js
+++ Frontend/src/pages/gameSearchPage.js
@@ -53,7 +53,7 @@ class GameSearchPage extends BaseClass {
             noGamesSection.classList.remove("active")
             getAllUsers.classList.remove("active")
             getAllGames.classList.remove("active")
-            await this.renderPopUpSearch();
+            await this.showPopUp(event);
         } else if (state === this.GET_ALL_GAMES) {
             popUpSearch.classList.remove("active")
             getAllGames.classList.add("active")
