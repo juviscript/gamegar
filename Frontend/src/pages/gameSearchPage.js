d2fa594cd4e065bf28eecd3e525cf67f09db83f2
juviscript <juvipana@gmail.com>
2023-04-13 14:44:59 -0400

    Created changes on gameSearchPage.js to implement search button.

--- Frontend/src/pages/gameSearchPage.js
+++ Frontend/src/pages/gameSearchPage.js
@@ -10,7 +10,7 @@ class GameSearchPage extends BaseClass {
 
     constructor() {
         super();
-        this.bindClassMethods(['onStateChange', 'renderFulLGameList', 'renderUserList', 'showUserList', 'showPopUp', 'showGameList', 'showDevInfo'], this);
+        this.bindClassMethods(['onStateChange', 'renderFulLGameList', 'renderUserList', 'onPopUpSubmit', 'showUserList', 'showPopUp', 'showGameList', 'showDevInfo'], this);
         this.dataStore = new DataStore();
 
         // Possible loading states of the page.
@@ -29,7 +29,8 @@ class GameSearchPage extends BaseClass {
         document.getElementById('all-games-button').addEventListener('click', this.showGameList);
         document.getElementById('search-button').addEventListener('click', this.showPopUp);
         document.getElementById('search-users-button').addEventListener('click', this.showUserList);
-        document.getElementById('about-button').addEventListener('click', this.showDevInfo)
+        document.getElementById('about-button').addEventListener('click', this.showDevInfo);
+        document.getElementById('pop-up-search-submit').addEventListener('submit', this.onPopUpSubmit);
         this.dataStore.addChangeListener(this.onStateChange);
 
         this.client = new VideoGameClient();
@@ -44,46 +45,47 @@ class GameSearchPage extends BaseClass {
     async onStateChange() {
         const state = this.dataStore.get("state");
 
-        const popUpSearch = document.getElementById("pop-up-search")
-        const getAllGames = document.getElementById("games-list")
-        const noGamesSection = document.getElementById("empty-games")
-        const getAllUsers = document.getElementById("user-list")
+        const popUpSearch = document.getElementById("pop-up-search");
+        const getAllGames = document.getElementById("games-list");
+        const noGamesSection = document.getElementById("empty-games");
+        const getAllUsers = document.getElementById("user-list");
         const showDevInfo = document.getElementById("about-the-devs")
-
+;
         if (state === this.POP_UP_SEARCH) {
-            popUpSearch.classList.add("active")
-            getAllGames.classList.remove("active")
-            noGamesSection.classList.remove("active")
-            getAllUsers.classList.remove("active")
-            getAllGames.classList.remove("active")
+            popUpSearch.classList.add("active");
+            getAllGames.classList.remove("active");
+            noGamesSection.classList.remove("active");
+            getAllUsers.classList.remove("active");
+            getAllGames.classList.remove("active");
+            showDevInfo.classList.remove("active");
+            // await this.renderPopUpSearch();
             await this.showPopUp(event);
-            await this.renderPopUpSearch();
         } else if (state === this.GET_ALL_GAMES) {
-            popUpSearch.classList.remove("active")
-            getAllGames.classList.add("active")
-            getAllUsers.classList.remove("active")
-            noGamesSection.classList.remove("active")
-            showDevInfo.classList.remove("active")
+            popUpSearch.classList.remove("active");
+            getAllGames.classList.add("active");
+            getAllUsers.classList.remove("active");
+            noGamesSection.classList.remove("active");
+            showDevInfo.classList.remove("active");
             await this.renderFulLGameList(); //renders gamelist
         } else if (state === this.NO_GAMES) {
-            popUpSearch.classList.remove("active")
-            getAllGames.classList.remove("active")
-            noGamesSection.classList.add("active")
-            getAllUsers.classList.remove("active")
-            showDevInfo.classList.remove("active")
+            popUpSearch.classList.remove("active");
+            getAllGames.classList.remove("active");
+            noGamesSection.classList.add("active");
+            getAllUsers.classList.remove("active");
+            showDevInfo.classList.remove("active");
         } else if (state === this.GET_ALL_USERS) {
-            popUpSearch.classList.remove("active")
-            getAllGames.classList.remove("active")
-            noGamesSection.classList.remove("active")
-            getAllUsers.classList.add("active")
-            showDevInfo.classList.remove("active")
+            popUpSearch.classList.remove("active");
+            getAllGames.classList.remove("active");
+            noGamesSection.classList.remove("active");
+            getAllUsers.classList.add("active");
+            showDevInfo.classList.remove("active");
             await this.renderUserList(); //renders userlist
         } else if (state === this.SHOW_DEV_INFO) {
-            popUpSearch.classList.remove("active")
-            getAllGames.classList.remove("active")
-            noGamesSection.classList.remove("active")
-            getAllUsers.classList.remove("active")
-            showDevInfo.classList.add("active")
+            popUpSearch.classList.remove("active");
+            getAllGames.classList.remove("active");
+            noGamesSection.classList.remove("active");
+            getAllUsers.classList.remove("active");
+            showDevInfo.classList.add("active");
         }
     }
 
@@ -94,12 +96,13 @@ class GameSearchPage extends BaseClass {
 
 
     async renderFulLGameList() {
+        console.log("Rendering full game list...")
         let gameHtml = "";                              // Variable named gameHtml that starts as an empty string of HTML information.
         const games = this.dataStore.get("games");
 
         for (let i = 0; i < games.length; i++) {
 
-            gameHtml += ` 
+            gameHtml += `
 
                 <div class = "card">
                     <div id = "game-image-container">
@@ -128,7 +131,7 @@ class GameSearchPage extends BaseClass {
                     </div>
 
                 </div>
-      
+
                 `;
 
         }
@@ -138,7 +141,6 @@ class GameSearchPage extends BaseClass {
 
 
     async renderUserList() {
-
         console.log("User list rendering...");
         let userHtml = "";
 
@@ -174,31 +176,81 @@ class GameSearchPage extends BaseClass {
 
 
 
+
     // Event Handlers --------------------------------------------------------------------------------------------------
 
      async showGameList() {
+            console.log("Triggering full game list...")
             const games = await this.client.getAllGames();
             this.dataStore.set("games", games);
             this.dataStore.set("state", this.GET_ALL_GAMES);
      }
 
      async showPopUp(event) {
-         console.log("Rendering pop up search...");
+         console.log("Triggering pop up search...");
 
          event.preventDefault();
          this.dataStore.set("state", this.POP_UP_SEARCH);
      }
 
     async showUserList() {
+        console.log("Triggering full user list...")
         const users = await this.userClient.getAllUsers(this.errorHandler);
         this.dataStore.set("users", users);
         this.dataStore.set("state", this.GET_ALL_USERS);
     }
 
     async showDevInfo(event) {
+        console.log("Triggering dev information...")
         event.preventDefault();
         this.dataStore.set("state", this.SHOW_DEV_INFO)
     }
+
+    async onPopUpSubmit{
+            console.log("Pop up search rendering...");
+            let gameHtml = "";
+            let gameId = document.getElementById("game-search-id").value;
+            const games = this.dataStore.get("games");
+
+
+            for (const game of games) {
+                if (gameId === game.id)
+
+                        gameHtml += `
+
+                            <div class = "card">
+                                <div id = "game-image-container">
+                                    <img src = ${game.image} id = "game-img">
+                                </div>
+
+                                <div id = "game-details">
+                                    <p id = "info-2">
+                                      <h1> ${games.title} </h1>
+                                    <b>Developer:</b> ${game.developer}
+                                    <br>
+                                    <b>Country of Origin:</b> ${game.country}
+                                    <br>
+                                    <b>Year:</b> ${game.year}
+                                    <br>
+                                    <b>Genre:</b> ${gamegenre}
+                                    <br>
+                                    <b>Platforms:</b> ${game.platforms}
+                                    <br>
+                                    <b>Tags:</b> ${game.tags}
+                                    </p>
+                                    <br>
+                                    <p id = "description">
+                                        ${game.description}
+                                    </p>
+                                </div>
+
+                            </div>
+                            `;
+                    }
+
+                    document.getElementById("pop-up-search-list").innerHTML = gameHtml;
+
+        }
 
 }
 
